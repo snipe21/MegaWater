8ce436aecd96de9336072841734182d0fba1feea
snipe21 <7beachrd@gmail.com>
2026-08-24 12:40:38 +0200

    Delete backend directory

--- backend/qr.py
+++
@@ -1,9 +0,0 @@
-import io
-import qrcode
-
-
-def qr_png_bytes(data: str) -> bytes:
-    img = qrcode.make(data, box_size=8, border=2)
-    buf = io.BytesIO()
-    img.save(buf, format="PNG")
-    return buf.getvalue()
